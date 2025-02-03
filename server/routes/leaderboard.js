e91c3f0c26a4e2e5f6ad217963f6e0c726114ada
Muhammad Rizwan <muhammad.rizwan@moniepoint.com>
2025-02-03 21:12:16 +0500

    win percentage

--- server/routes/leaderboard.js
+++ server/routes/leaderboard.js
@@ -5,8 +5,16 @@ const User = require("../models/User");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const leaderboard = await User.find().sort({ highScore: -1 }).limit(10);
-  res.json(leaderboard);
+  try {
+    const leaderboard = await User.find({ highScore: { $gt: 0 } })
+                                  .sort({ highScore: -1 })
+                                  .limit(50)
+                                  .select('username highScore winPercentage');
+
+    res.json(leaderboard);
+  } catch (err) {
+    res.status(500).json({ error: "Something went wrong while fetching the leaderboard." });
+  }
 });
 
 module.exports = router;
