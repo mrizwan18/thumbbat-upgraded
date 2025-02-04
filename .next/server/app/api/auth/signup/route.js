3ac43ff6cd82a1c76931f085160caa994c6f6cc6
alikonainofficial <worksbetter12@gmail.com>
2025-02-04 23:03:44 +0300

    Remove .next build directory from version control

--- .next/server/app/api/auth/signup/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/auth/signup/route.js";
-const runtime = require("../../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_2c5310._.js");
-runtime.loadChunk("server/chunks/[root of the server]__41874b._.js");
-runtime.loadChunk("server/chunks/_27ca83._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/auth/signup/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/auth/signup/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
