f817e74b1c4c4129d2ed53cca7a31e6e30b16cd5
Adeela Abid <saeedrasool76@gmail.com>
2025-02-11 21:48:06 +0100

    Chore: RoundOff leaderboard winning percentage

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -100,7 +100,7 @@ const Leaderboard = () => {
                     </td>
                     <td className="p-4">{player.username}</td>
                     <td className="p-4 text-center">{player.highScore}</td>
-                    <td className="p-4 text-center">{player.winPercentage.toFixed(2)}%</td>
+                    <td className="p-4 text-center">{Math.round(player.winPercentage)}%</td>
                   </motion.tr>
                 );
               })}
